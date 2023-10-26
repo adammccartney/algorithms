2d2cd9a398dd9d8e8a440c421c04b45038960d32
Adam McCartney <adam@mur.at>
2023-10-26 18:04:10 +0200

    Add distclean to makefiles
    Make it easier to remove the libraries after they are
    installed.

--- libs/adlib/ad.c
+++ libs/adlib/ad.c
@@ -1,4 +1,6 @@
-#include "adlib.h"
+#include "ad.h"
+
+/* Queue */
 
 queue* initQueue (int size) {
     queue* q = (queue*) malloc(sizeof(queue) + sizeof(int) * size);
@@ -38,3 +40,67 @@ int dequeue(queue* q) {
     q->top--;
     return result;
 }
+
+/* Graph */
+
+void initialize_graph(graph* g, bool directed) {
+    int i;                 /* Counter */
+    
+    g->nvertices = 0;
+    g->nedges = 0;
+    g->directed = directed;
+
+    for (i=1; i<=MAXV; i++) g->degree[i] = 0;
+    for (i=1; i<=MAXV; i++) g->edges[i] = NULL;
+}
+
+void read_graph(graph* g, bool directed) {
+    int i;                 /* Counter */
+    int m;                 /* number of edges */
+    int x, y;              /* vertices in edge (x,y) */
+
+    initialize_graph(g, directed);
+
+    scanf("%d %d",&(g->nvertices),&m);
+
+    for (i=1; i<=m; i++) {
+        scanf("%d %d",&x,&y);
+        insert_edge(g,x,y,directed);
+    }
+}
+
+void insert_edge(graph* g, int x, int y, bool directed) {
+    edgenode* p;           /* temporary pointer */
+
+    p = malloc(sizeof(edgenode)); /* allocate edgenode storage */
+
+    p->weight = 0;
+    p->y = y;
+    p->next = g->edges[x];
+
+    g->edges[x] = p;       /* insert at head of list */
+
+    g->degree[x]++;
+
+    if (directed == FALSE)
+        insert_edge(g,y,x,TRUE);
+    else
+        g->nedges++;
+}
+
+void print_graph(graph* g) {
+    int i;               /* Counter */
+    edgenode *p;         /* temporary pointer */
+
+    for (i=1; i<=g->nvertices; i++) {
+        printf("%d: ", i);
+        p = g->edges[i];
+        while (p != NULL) {
+            printf(" %d", p->y);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
+
+
